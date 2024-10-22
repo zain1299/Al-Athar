bfa8e16928b79b548304db5a032d814755025236
SMH <SMH@001-2538>
2024-10-22 15:26:44 +0400

    new

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    domain: 'http://185.64.26.115/Central-Meeting/api/',
+    //domain: 'http://185.64.26.115/Central-Meeting/api/',
+    domain: 'http://10.241.115.42/Central-Meeting/api/',
 };
