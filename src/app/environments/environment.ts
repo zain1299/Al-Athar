037b1abe699cb106bdf8afe2bb41b098d417d869
Zain Ul Abideen <zainulabideen@digitalgravity.ae>
2025-10-08 01:33:25 +0500

    ..

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     domain: 'https://stmoi.moi.gov.om/central-meeting/api/',
-    // domain: 'http://10.241.115.42/Central-Meeting/api/',
     contractHostURL: 'http://localhost:5017/api/',
 };
